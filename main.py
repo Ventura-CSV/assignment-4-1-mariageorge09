21928003a6a285395faad2929652e40a99b5f654
Maria <emailtomariageorge@gmail.com>
2024-06-27 18:00:37 -0700

    4-1

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 def main():
     result = []
-    while True:
+    valid = False
+    while valid == False:
         start = input('Enter the starting letter: ')
-        end = input('Enter the starting letter: ')
-
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
-
-    print(*result)
+        end = input('Enter the ending letter: ')
+        if start>end:
+            print ('error message')
+            valid = False
+        else: 
+            if start.isalpha() == False or end.isalpha() == False:
+                valid = False
+            else: 
+                valid = True
+                print (valid)
+                break
+    
+    begin = ord(start)
+    last = ord(end)
+    for num in range(begin, last +1 , 1):     
+        result.append(chr(num))
+    print(result)
 
     ########################################
     # Do not delete the return statement
     ########################################
-    return result
+    return result,valid
 
 
 if __name__ == '__main__':
